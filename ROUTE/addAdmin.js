6dcb8f4ac0bcfb260d55463d5c6323438c95caf4
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2023-02-13 20:30:55 +0100

    forget Password

--- ROUTE/addAdmin.js
+++ ROUTE/addAdmin.js
@@ -6,10 +6,8 @@ const Router = express.Router();
 Router.route('/Login').post(Adminlogin)
 Router.route('/adminSign').post(AdminSignUp)
 Router.route('/Verify/:Id').post(AdminVerify)
-Router.route('/confirmAdmin/:userid').post(isAdminVerify)
-Router.route('/Forget/:id').post(Forgotpassword)
+Router.route('/confirmAdmin/:Id').post(isAdminVerify)
+Router.route('/Forget').post(Forgotpassword)
 Router.route('/Chng/:id').post(passwordchange)
 //Router.route('/adminChng/:id/:token').post(passwordchange)
 module.exports = Router;
-
-
