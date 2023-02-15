74f2fe77d508d2dcbdf30d28c0393859eb8b4df2
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2023-02-15 13:55:51 +0100

    commit

--- models/product.js
+++ models/product.js
@@ -1,36 +1,40 @@
 const mongoose = require('mongoose')
 const productSchema = mongoose.Schema({
-    title: {
-      type: String,
-      required: [true, "title is required"]
-    },
-    description: {
-      type: String,
-      required: [true, "description is required"],
-    },
-    image: {
-      type: String,
-    },
-    cloudId: {
-      type: String,
-    },
-    price: {
-      type: String,
-      required: [true, "price is required"],
-    },
-    rating: {
-      type: Number,
-      default: 0, 
-    },
-    stockQuantity: {
-      type: String,
-      required: [true, "stockQuantity is required"]
-    },
-    brandName: {
-      type: String,
-      required: [true, "brandName is required"]
-    }, 
+  title: {
+    type: String,
+    required: [true, "title is required"]
   },
+  description: {
+    type: String,
+    required: [true, "description is required"],
+  },
+  image: {
+    type: String,
+  },
+  cloudId: {
+    type: String,
+  },
+  price: {
+    type: String,
+    required: [true, "price is required"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  categories: {
+    type: String,
+    required: [true, "stockQuantity is required"]
+  },
+  stockQuantity: {
+    type: String,
+    required: [true, "stockQuantity is required"]
+  },
+  brandName: {
+    type: String,
+    required: [true, "brandName is required"]
+  },
+},
   {
     timestamps: true
   })
