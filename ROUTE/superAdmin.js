dc254f31635e3babc8288c52a0b89c9608e1e55c
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2023-02-28 19:36:24 +0100

    delete

--- ROUTE/superAdmin.js
+++ ROUTE/superAdmin.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const {SuperASignUp,getAllUsers,getAllAdmin} = require("../controller/admin")
+const { SuperASignUp, getAllUsers, getAllAdmin, DelUser } = require("../controller/admin")
 //const {isSuperAdmin} = require('../helper/auth')
 const superRoutes = express.Router();
 
 superRoutes.route("/super").post(SuperASignUp)
 superRoutes.route('/allUsers/:id').get(getAllUsers)
 superRoutes.route('/allAdmin/:id').get(getAllAdmin)
-superRoutes.route('/delete/:id').get(getAllAdmin)
+superRoutes.route('/delete/:userId').delete(DelUser)
 
 module.exports = superRoutes
