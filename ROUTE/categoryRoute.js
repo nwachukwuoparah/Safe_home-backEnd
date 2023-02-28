72404b941421bdba356f406699fde79b6d02c7d4
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2023-02-28 17:59:44 +0100

    change

--- ROUTE/categoryRoute.js
+++ ROUTE/categoryRoute.js
@@ -2,13 +2,14 @@ const express = require("express")
 
 const cateRouter = express.Router();
 
-const {NewCates,getAllCates,getCategoryByName} = require('../controller/category');
+const {NewCates,getAllCates,getCategoryByName} = require('../controller/cate');
 //const {IsSuperAdmin} = require("../helper/auth")
-
-
 
 
 cateRouter.route("/cates").post(NewCates);
 cateRouter.route("/allCates/category").get(getAllCates)
+
+
+
 cateRouter.route("/categories/:name").get(getCategoryByName)
 module.exports = cateRouter;
