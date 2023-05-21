5baaec2b648d0c328ae0f2bced5f5fec866a29fd
nwachukwuoparah <nwachukwuoparah@gmail.com>
2023-05-21 13:59:37 +0100

    email

--- tils/Emails.js
+++ tils/Emails.js
@@ -1,29 +1,30 @@
 const dotenv = require("dotenv");
 const nodemailer = require("nodemailer");
-dotenv.config({path: './CONFIG/config.env'})
+dotenv.config({ path: "./CONFIG/config.env" });
 
+const mailSender = async (option) => {
+  try {
+    const transporter = nodemailer.createTransport({
+            service: process.env.SERVICE, 
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAILPASSWORD,
+        secure: false,
+      },
+    });
 
-
-const mailSender = async(option)=>{
-    try {
-        const transporter = nodemailer.createTransport({
-            service: process.env.SERVICE,
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAILPASSWORD,
-                secure: false
-            },
-        })
+    const mailOption = {
+      from: process.env.EMAIL,
+      to: option.email,
+      subject: option.subject,
+      text: option.message,
+    };
     
-        const mailOption = {
-            from: process.env.EMAIL,
-            to: option.email,
-            subject:option.subject,
-            text:option.message
-        };
-        await transporter.sendMail(mailOption);
-    } catch (error) {
-        console.log('error from mail sender=> ', error)
-    }
- }
+
+   const send =  await transporter.sendMail(mailOption);
+
+  } catch (error) {
+    console.log("error from mail sender=> ", error);
+  }
+};
 module.exports = mailSender;
